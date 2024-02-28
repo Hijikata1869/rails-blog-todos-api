8f60d0e74bccbe68781884f09506e81d6b426feb
Hijikata1869 <liv.knk@gmail.com>
2024-02-28 20:39:11 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_secure_password
+
+  validates :username, length: { maximum: 18 }, presence: true
+  validates :password_digest, presence: true
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  namespace :api do
+    resources :users, only: %i[create]
+  end
+end
