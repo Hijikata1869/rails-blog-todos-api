0a45e4b370d8052c765ccda569d70478ea46c0ec
Hijikata1869 <liv.knk@gmail.com>
2024-03-03 13:18:20 +0900

    Blogデータを作成

---
+++ app/controllers/api/blogs_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  class BlogsController < ApplicationController 
+    def index
+      blogs = Blog.all
+      render json: blogs, status: :ok
+    end
+
+    def show
+      blog = Blog.find(params[:id])
+      render json: blog, status: :ok
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: %i[create]
+    resources :blogs, only: [:index, :show]
     post 'auth', to: 'auth#create'
   end
 end
