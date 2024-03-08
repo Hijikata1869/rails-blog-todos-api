43c0ff4b819c464de783793a793bef3622a421ad
Hijikata1869 <liv.knk@gmail.com>
2024-03-08 18:42:20 +0900

    taskのcreateメソッドと、updateメソッドを実装

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,15 +1,33 @@
 module Api
   class TasksController < ApplicationController
-    before_action :authenticate_user, only: [:destroy]
+    before_action :authenticate_user, only: [:create, :destroy]
 
     def index
       tasks = Task.all
       render json: tasks, status: :ok
     end
 
+    def create 
+      task = Task.new(task_params)
+      if task.create
+        render json: {}, status: :ok
+      else
+        render json: {}, status: 422
+      end
+    end
+
     def show
       task = Task.find(params[:id])
       render json: task, stauts: :ok
+    end
+
+    def update
+      task = Taks.find(params[:id])
+      if task.update(title: params[:title])
+        render json: {}, status: :ok
+      else
+        render json: {}, stauts: 422
+      end
     end
 
     def destroy
@@ -20,5 +38,11 @@ module Api
         render json: {}, status: 401
       end
     end
+
+    private
+    def task_params
+      params.require(:task).permit(:title)
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: %i[create]
     resources :blogs, only: [:index, :show]
-    resources :tasks, only: [:index, :show, :destroy]
+    resources :tasks, except: [:new, :edit]
     post 'auth', to: 'auth#create'
   end
 end
